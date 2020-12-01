184da107a83a70078a1fcbb73093042bd324dcdb
j-brn <me@jbrn.eu>
2020-12-01 13:03:33 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# advent of code 2020
+
+## How to run?
+
+1. install [cargo-aoc](https://github.com/gobanos/cargo-aoc)
+2. provide cargo-aoc with credentials 
+   ```
+   cargo aoc credential -s <your advent of code session cookie>
+   ```
+3. download your input
+   ```
+   cargo aoc input
+   ```
+4. run
+   ```
+   # run a specific day/part
+   cargo aoc -d <day> -p <part>
+   
+   # run everything
+   cargo aoc
+   
+   # run benchmarks
+   cargo aoc bench -d <day> -p <part> 
+   ```
---
+++ src/day1.rs
@@ -0,0 +1,53 @@
+use aoc_runner_derive::{aoc, aoc_generator};
+use itertools::Itertools as _;
+
+#[aoc_generator(day1)]
+fn input_generator(input: &str) -> Vec<u32> {
+    input
+        .lines()
+        .map(|line| line.parse::<u32>().unwrap())
+        .collect()
+}
+
+/// find a pair of numbers that sums up to `sum`
+fn find_pair(mut numbers: Vec<u32>, sum: u32) -> Option<(u32, u32)> {
+    let (mut l, mut r) = (0, numbers.len() - 1);
+    numbers.sort();
+
+    while l < r {
+        match (numbers[l], numbers[r]) {
+            (a, b) if a + b == sum => return Some((a, b)),
+            (a, b) if a + b < sum => l += 1,
+            _ => r -= 1,
+        }
+    }
+
+    None
+}
+
+/// Find a set of `n_summands` that sums up to `sum` by bruteforcing them.
+fn find_summands(numbers: &[u32], n_summands: usize, sum: u32) -> Option<Vec<u32>> {
+    let min = numbers.iter().copied().min().unwrap_or(u32::min_value());
+
+    numbers
+        .iter()
+        .copied()
+        .filter(|x| *x < 2020 - min)
+        .combinations(n_summands)
+        .find(|x| x.iter().sum::<u32>() == sum)
+}
+
+#[aoc(day1, part1)]
+pub fn solve_part_1(numbers: &[u32]) -> u32 {
+    let (a, b) = find_pair(numbers.to_vec(), 2020).expect("no solution found");
+
+    a * b
+}
+
+#[aoc(day1, part2)]
+pub fn solve_part_2(numbers: &[u32]) -> u32 {
+    find_summands(numbers, 3, 2020)
+        .expect("no solution found")
+        .iter()
+        .product()
+}
